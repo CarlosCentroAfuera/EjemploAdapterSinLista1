c3b594b660e051f301fb0a808206c425da290545
Carlos <carlostena@centroafuera.es>
2021-06-16 13:36:14 +0200

    Updated to match with parent exercise

--- app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
+++ app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
@@ -1,36 +1,42 @@
 package com.cbellmont.ejemploadaptersinlista
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.cbellmont.ejemploadaptersinlista.databinding.ItemBinding
 
 class StringAdapter : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
 
-    class StringViewHolder(var root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
+    lateinit var binding : ItemBinding
 
-    var numeroDeFilas = 10
+    class StringViewHolder(var itemBinding : ItemBinding) : RecyclerView.ViewHolder(itemBinding.root)
 
+    var numeroDeFilas = 20
+
+    // Aquí solo obtenemos las vistas que están dentro de layout en el que queremos mostrar nuestros elementos
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
-        val twTextView = view.findViewById<TextView>(R.id.textView)
-        return StringViewHolder(view, twTextView)
+        binding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StringViewHolder(binding)
     }
 
+    // Aquí devolvemos el número de elementos que habrá en nuestro recylcerView
     override fun getItemCount(): Int {
-        return numeroDeFilas
+        return numeroDeFilas + 1
     }
 
+    // Aquí ponermos en el layout vacío que hemos creado en el onCreateViewHolder los datos del elemento
     override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
-        holder.textView.text = if (position % 2 == 0) {
-            holder.textView.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.colorPrimaryDark))
+        holder.itemBinding.textView.text = if (position % 2 == 0) {
+            holder.itemBinding.textView.setBackgroundColor(ContextCompat.getColor(holder.itemBinding.root.context, R.color.colorPrimaryDark))
             "Position = $position"
         } else {
-            holder.textView.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.colorAccent))
+            holder.itemBinding.textView.setBackgroundColor(ContextCompat.getColor(holder.itemBinding.root.context, R.color.colorAccent))
             "Soy impar"
+        }
+
+        if (position == numeroDeFilas) {
+            holder.itemBinding.textView.text = "Soy el último"
         }
     }
 
